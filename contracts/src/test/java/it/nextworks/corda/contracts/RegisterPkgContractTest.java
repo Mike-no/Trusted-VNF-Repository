f3f006f3c956672bbb0c50682022ad65a394c5e3
Mike-no <deangelismichael98@gmail.com>
2020-10-17 19:02:56 +0200

    some refactoring; add FeeAgreementState, FeeAgreementContract, EstablishFeeAgreementContractTest classes.

--- contracts/src/test/java/it/nextworks/corda/contracts/RegisterPkgContractTest.java
+++ contracts/src/test/java/it/nextworks/corda/contracts/RegisterPkgContractTest.java
@@ -11,7 +11,7 @@ import static it.nextworks.corda.contracts.PkgOfferUtils.*;
 import static net.corda.testing.node.NodeTestUtils.ledger;
 
 /** Test PkgOfferContract class in cases where the command used in the transaction is RegisterPkg */
-public class CreatePkgOfferContractTest {
+public class RegisterPkgContractTest {
 
     /** Simulate a Corda Network composed by two nodes: a developer and the repositoryNode */
     private static final TestIdentity devTest =
@@ -248,6 +248,7 @@ public class CreatePkgOfferContractTest {
         }));
     }
 
+    /** Test that the <price> parameter of the output state of a transaction must not be null */
     @Test
     public void stateMustHaveValidPrice() {
         ledger(ledgerServices, (ledger -> {
@@ -265,6 +266,7 @@ public class CreatePkgOfferContractTest {
         }));
     }
 
+    /** Test that the <pkgType> parameter of the output state of a transaction must not be null */
     @Test
     public void stateMustHaveValidPkgType() {
         ledger(ledgerServices, (ledger -> {
